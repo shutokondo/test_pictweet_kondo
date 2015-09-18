fae73e6f3360099561e183ff78272c0744ec90ce
根東秀都 <shuto8201@gmail.com>
2015-09-19 01:51:17 +0900

    リファクタリング

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    nickname { Faker::Lorem.characters(6) }
+    nickname { Faker::Lorem.characters(1..6) }
     email { Faker::Internet.email }
-    password { Faker::Internet.password }
+    password { Faker::Internet.password(8) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,43 +1,48 @@
 require 'rails_helper'
 
 describe User do
-  it "is invalid with nickname over 6 letters" do
-    user = build(:user, nickname: 'shutokondo')
-    user.valid?
-    expect(user.errors[:nickname]).to include('is too long (maximum is 6 characters)')
-  end
+  describe 'validation' do
+    it "is invalid with nickname over 6 letters" do
+      user = build(:user, nickname: 'shutokondo')
+      user.valid?
+      expect(user.errors[:nickname]).to include('is too long (maximum is 6 characters)')
+    end
 
-  #nicknameとemailがあれば有効
-  it "is valid with nickname and email" do
-    user = build(:user)
-    expect(user).to be_valid
-  end
+    #nicknameとemailがあれば有効
+    it "is valid with nickname and email" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
 
-  #emailが空だと無効
-  it "is invalid without email" do
-    user = build(:user, email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
-  end
+    #emailが空だと無効
+    it "is invalid without email" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
 
-  #nicknameが空だと無効
-  it "is invalid without nickname" do
-    user = build(:user, nickname: nil)
-    user.valid?
-    expect(user.errors[:nickname]).to include("can't be blank")
-  end
+    #nicknameが空だと無効
+    it "is invalid without nickname" do
+      user = build(:user, nickname: nil)
+      user.valid?
+      expect(user.errors[:nickname]).to include("can't be blank")
+    end
 
-  #重複したemailは無効
-  it "is invalid with a duplicate email" do
-    create(:user, email: 'test@gmail.com')
-    user = build(:user, email: 'test@gmail.com')
-    user.valid?
-    expect(user.errors[:email]).to include("has already been taken")
+    #重複したemailは無効
+    it "is invalid with a duplicate email" do
+      create(:user, email: 'test@gmail.com')
+      user = build(:user, email: 'test@gmail.com')
+      user.valid?
+      expect(user.errors[:email]).to include("has already been taken")
+    end
   end
-
-  it "returns a tweet's count of current_user" do
-    user = create(:user)
-    tweet = create(:tweet)
-    expect(user.count_tweets).to eq user.count_tweets
+  describe '#count_tweets' do
+    let(:user) { create(:user) }
+    before do
+      3.times{ create(:tweet, user: user) }
+    end
+    it 'returns 3' do
+      expect(user.count_tweets).to eq 3
+    end
   end
 end
